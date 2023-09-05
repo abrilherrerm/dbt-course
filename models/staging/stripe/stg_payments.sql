f8dbb5eb0446059a9b891053e71ba4be73b5c525
Abril Herrera <abrilbeatrizh@gmail.com>
2023-09-05 23:11:04 +0000

    final commit of the day

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,8 +1,11 @@
 select
     id as payment_id,
-    orderid as order_id
-   paymentmethod as payment_method,
+    orderid as order_id,
+    paymentmethod as payment_method,
     status,
-    amount/100 as amount
+
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
     created as created_at
-from raw.stripe.payment
+
+from raw.stripe.payment 
